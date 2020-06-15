d87ae5efec5c5d938fd84f2b668afc45ebcd6c44
Sanks <sanks64@protonmail.com>
2020-06-15 21:55:04 +0600

    improve crystal

--- crystal.cr
+++ crystal.cr
@@ -1,16 +1,16 @@
 user = ENV["USER"]
-host = File.read("/etc/hostname").strip()
-kernel = File.read("/proc/version").split()[2]
+host = System.hostname
+kernel = File.read("/proc/version").split[2]
 term = ENV["TERM"]
 shell = ENV["SHELL"]
 tasks = Dir.entries("/proc").count { |x| x.to_i? }
 
-mem = File.read("/proc/meminfo").lines()
-def getsize(s) s.split()[1].to_i // 1000 end
+mem = File.read("/proc/meminfo").lines
+def getsize(s) s.split[1].to_i // 1000 end
 total = getsize(mem[0])
 avail = getsize(mem[2])
 
-uptime = File.read("/proc/uptime").split()[0].to_f
+uptime = File.read("/proc/uptime").split[0].to_f
 d = (uptime / 60 / 60 / 24).to_i
 h = (uptime / 60 / 60 % 24).to_i
 m = (uptime / 60 % 60).to_i
