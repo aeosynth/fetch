3e88e00ee04728f1739cf7fa2696d658303430a9
Recidiviste <luc.saccoccio@gmail.com>
2020-06-27 11:06:27 +0200

    At least it works

--- perl.pl
+++ perl.pl
@@ -13,6 +13,12 @@ open("f", "<", "/proc/uptime");
 my @times = split / /, <f>;
 close "f";
 
+open("f", "<", "/proc/meminfo");
+my @fi = <f>;
+close "f";
+( my $total ) = $fi[0] =~ /(\d+)/;
+( my $avail ) = $fi[2] =~ /(\d+)/;
+
 opendir my($dh), "/proc";
 my @tasks = grep { /^[+-]?\d+$/ } readdir $dh;
 closedir $dh;
@@ -20,9 +26,9 @@ closedir $dh;
 my $user = $ENV{'USER'};
 my $term = $ENV{'TERM'};
 my $shell = $ENV{'SHELL'};
-my $tasks = "";
-my $avail = "";
-my $total = "";
+my $tasks = $#tasks + 1;
+$total = int($total / 1000);
+$avail = int($avail / 1000);
 my $d = int((($times[0] / 60) / 60) / 24);
 my $h = (($times[0] / 60) / 60) % 24;
 my $m = ($times[0] / 60) % 60;
@@ -30,7 +36,7 @@ print $user . "@" . $hostname
 	. "kernel\t" . $version[2]
 	. "\nterm\t" . $term
 	. "\nshell\t" . $shell
-	. "\ntasks\t" . $#tasks
+	. "\ntasks\t" . ($#tasks + 1)
 	. "\nmem\t" . $avail . "m / " . $total . "m"
 	. "\nuptime\t" . $d . "d " . $h . "h " . $m . "m"
 	. "\n";
